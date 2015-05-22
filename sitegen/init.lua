a770e75d640f94cc8103c931185a70ea192bff74
leaf corcoran <leafot@gmail.com>
2015-05-22 12:26:51 -0700

    add new create function that doesn't add markdown, add warning to old method

--- sitegen/init.lua
+++ sitegen/init.lua
@@ -1,4 +1,5 @@
 local Site = require("sitegen.site")
+local colors = require("ansicolors")
 local insert
 insert = require("table").insert
 local default_plugins = {
@@ -17,12 +18,25 @@ create_site = function(init_fn, site)
   if site == nil then
     site = Site()
   end
+  io.stderr:write(colors("%{bright}%{red}WARNING: %{reset}sitegen.create_site is deprecated, use create and add markdown files manually.\n"))
   do
     local _with_0 = site
     _with_0:init_from_fn(init_fn)
     if not (_with_0.autoadd_disabled) then
       _with_0.scope:search("*md")
     end
+    return _with_0
+  end
+end
+local create
+create = function(init_fn, site)
+  if site == nil then
+    site = Site()
+  end
+  assert(init_fn, "Attempted to create site without initialization function")
+  do
+    local _with_0 = site
+    _with_0:init_from_fn(init_fn)
     return _with_0
   end
 end
@@ -39,6 +53,7 @@ for _index_0 = 1, #default_plugins do
 end
 return {
   create_site = create_site,
+  create = create,
   plugins = plugins,
   register_plugin = register_plugin
 }
--- sitegen/init.moon
+++ sitegen/init.moon
@@ -1,4 +1,6 @@
 Site = require "sitegen.site"
+colors = require "ansicolors"
+
 import insert from require "table"
 
 default_plugins = {
@@ -14,10 +16,17 @@ default_plugins = {
 
 plugins = {}
 
+-- legacy create that adds all md files
 create_site = (init_fn, site=Site!) ->
+  io.stderr\write colors "%{bright}%{red}WARNING: %{reset}sitegen.create_site is deprecated, use create and add markdown files manually.\n"
   with site
     \init_from_fn init_fn
     .scope\search "*md" unless .autoadd_disabled
+
+create = (init_fn, site=Site!) ->
+  assert init_fn, "Attempted to create site without initialization function"
+  with site
+    \init_from_fn init_fn
 
 register_plugin = (plugin) ->
   plugin\on_register! if plugin.on_register
@@ -28,5 +37,6 @@ for pname in *default_plugins
 
 {
   :create_site
+  :create
   :plugins, :register_plugin
 }
