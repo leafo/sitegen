ca068d815d9cf73348190cfddcb7d48bfce92a6f
leaf corcoran <leafot@gmail.com>
2015-07-01 22:38:53 -0700

    fix up deploy plugin

--- sitegen/plugins/deploy.lua
+++ sitegen/plugins/deploy.lua
@@ -7,8 +7,6 @@ do
     mixin_funcs = {
       "deploy_to"
     },
-    help = [[    This is how you use this plugin....
-  ]],
     deploy_to = function(self, host, path)
       if host == nil then
         host = error("need host")
@@ -17,6 +15,16 @@ do
         path = error("need path")
       end
       self.host, self.path = host, path
+    end,
+    sync = function(self)
+      assert(self.host, "missing host for deploy")
+      assert(self.path, "missing path for deploy")
+      return os.execute(table.concat({
+        'rsync -rvuzL www/ ',
+        self.host,
+        ':',
+        self.path
+      }))
     end
   }
   _base_0.__index = _base_0
@@ -44,15 +52,6 @@ do
     end
   })
   _base_0.__class = _class_0
-  local self = _class_0
-  self.sync = function(self, host, path)
-    return os.execute(table.concat({
-      'rsync -rvuzL www/ ',
-      host,
-      ':',
-      path
-    }))
-  end
   if _parent_0.__inherited then
     _parent_0.__inherited(_parent_0, _class_0)
   end
--- sitegen/plugins/deploy.moon
+++ sitegen/plugins/deploy.moon
@@ -1,17 +1,16 @@
 import Plugin from require "sitegen.plugin"
 
 class DeployPlugin extends Plugin
-  -- 'rsync -arvuz www/ leaf@leafo.net:www/test'
-  @sync = (host, path) =>
-    os.execute table.concat {
-      'rsync -rvuzL www/ ', host ,':', path
-    }
-
   mixin_funcs: { "deploy_to" }
 
-  help: [[
-    This is how you use this plugin....
-  ]]
-
   deploy_to: (@host=error"need host", @path=error"need path") =>
+
+  -- 'rsync -arvuz www/ leaf@leafo.net:www/test'
+  sync: =>
+    assert @host, "missing host for deploy"
+    assert @path, "missing path for deploy"
+
+    os.execute table.concat {
+      'rsync -rvuzL www/ ', @host ,':', @path
+    }
 
