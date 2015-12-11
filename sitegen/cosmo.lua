625a5eef4bce3282aedb55de71f0aac8d17350bb
leaf corcoran <leafot@gmail.com>
2015-12-11 12:08:16 -0800

    more lua 5.2 shim

--- sitegen/cosmo.lua
+++ sitegen/cosmo.lua
@@ -1,2 +1,7 @@
 _G.unpack = _G.unpack or require("sitegen.common").unpack
+_G.loadstring = _G.loadstring or function(str, chunkname)
+  return load(coroutine.wrap(function()
+    return coroutine.yield(str)
+  end), chunkname)
+end
 return require("cosmo")
--- sitegen/cosmo.moon
+++ sitegen/cosmo.moon
@@ -1,3 +1,7 @@
 _G.unpack or= require("sitegen.common").unpack
+
+_G.loadstring or= (str, chunkname) ->
+  load coroutine.wrap(-> coroutine.yield str), chunkname
+
 require "cosmo"
 
