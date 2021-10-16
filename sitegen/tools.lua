b2f17d85c68937f5bd298bd3537ad69176981431
leaf corcoran <leafot@gmail.com>
2021-10-16 12:33:14 -0700

    tools.system_command will escape shell arguments

--- sitegen/tools.lua
+++ sitegen/tools.lua
@@ -1,8 +1,10 @@
+local shell_escape
+shell_escape = require("sitegen.path").shell_escape
 local system_command
 system_command = function(cmd, ext)
   return function(self, input, output)
     output = output and self:real_output_path_for(output) or self:real_output_path_for(input, ext)
-    local real_cmd = cmd:format(input, output)
+    local real_cmd = cmd:format("'" .. tostring(shell_escape(input)) .. "'", "'" .. tostring(shell_escape(output)) .. "'")
     return real_cmd:match("^%w+"), real_cmd, os.execute(real_cmd)
   end
 end
--- sitegen/tools.moon
+++ sitegen/tools.moon
@@ -1,8 +1,10 @@
 
+import shell_escape from require "sitegen.path"
+
 system_command = (cmd, ext) ->
   (input, output) =>
     output = output and @real_output_path_for(output) or @real_output_path_for(input, ext)
-    real_cmd = cmd\format input, output
+    real_cmd = cmd\format "'#{shell_escape(input)}'", "'#{shell_escape(output)}'"
     real_cmd\match"^%w+", real_cmd, os.execute real_cmd
 
 {
