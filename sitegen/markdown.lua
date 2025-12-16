bb1d843bf064240cc434209e538db57e734e33be
leaf corcoran <leafot@gmail.com>
2025-12-16 13:45:56 -0800

    add set_backend for markdown, lazily initialize discount

--- sitegen/markdown.lua
+++ sitegen/markdown.lua
@@ -1,10 +1,31 @@
-local discount = require("discount")
-local renderer
-renderer = function(md_source)
-  return assert(discount(md_source))
+local renderer = nil
+local set_backend
+set_backend = function(name, ...)
+  local _exp_0 = name
+  if "discount" == _exp_0 then
+    local opts_list = {
+      ...
+    }
+    local discount = require("discount")
+    renderer = function(md_source)
+      return assert(discount(md_source, unpack(opts_list)))
+    end
+  elseif "cmark" == _exp_0 then
+    local cmark = require("cmark")
+    local opts = opts or cmark.OPT_DEFAULT
+    renderer = function(md_source)
+      local document = assert(cmark.parse_string(md_source, opts))
+      return assert(cmark.render_html(document, opts))
+    end
+  else
+    return error("unknown markdown backend: " .. tostring(name))
+  end
 end
 local render
 render = function(md_source)
+  if not (renderer) then
+    set_backend("discount")
+  end
   return renderer(md_source)
 end
 local set_renderer
@@ -14,5 +35,6 @@ set_renderer = function(fn)
 end
 return {
   render = render,
-  set_renderer = set_renderer
+  set_renderer = set_renderer,
+  set_backend = set_backend
 }
--- sitegen/markdown.moon
+++ sitegen/markdown.moon
@@ -1,13 +1,30 @@
-discount = require "discount"
+renderer = nil
 
-renderer = (md_source) ->
-  assert discount md_source
+set_backend = (name, ...) ->
+  switch name
+    when "discount"
+      opts_list = {...}
+      discount = require "discount"
+      renderer = (md_source) ->
+        assert discount md_source, unpack opts_list
+    when "cmark"
+      cmark = require "cmark"
+      opts or= cmark.OPT_DEFAULT
+
+      renderer = (md_source) ->
+        document = assert cmark.parse_string md_source, opts
+        assert cmark.render_html document, opts
+    else
+      error "unknown markdown backend: #{name}"
 
 render = (md_source) ->
+  unless renderer
+    set_backend "discount"
   renderer md_source
 
 set_renderer = (fn) ->
   assert type(fn) == "function", "renderer must be a function"
   renderer = fn
 
-{ :render, :set_renderer }
+
+{ :render, :set_renderer, :set_backend }
